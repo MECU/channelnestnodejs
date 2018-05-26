a68f5a060063f161931b4f0cf64e98dabeffc13b
Michael Hoppes <hoppes@gmail.com>
2018-05-25 20:13:10 -0600

    Copy Heroku nodejs starter

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var msg = 'Hello World';
-console.log(msg);
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-console.debug('help');
-
-msg.toUpperCase;
-console.log(msg);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
